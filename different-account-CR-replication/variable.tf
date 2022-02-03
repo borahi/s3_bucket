12066767f7ddc350a7f0077a473f6739a2b8dfef
borahi <91128514+borahi@users.noreply.github.com>
2022-02-03 16:14:19 +0530

    Create variable.tf

---
+++ different-account-CR-replication/variable.tf
@@ -0,0 +1,19 @@
+
+variable "source_account" {
+    default = "aws-Dev"
+  
+}
+
+variable "destination_account" {
+    default = "default"
+  
+}
+
+variable "source_account_id" {
+    default = "***********"
+}
+
+variable "dest_account_id" {
+    default = "xxxxxxxxxxx" 
+}
+
